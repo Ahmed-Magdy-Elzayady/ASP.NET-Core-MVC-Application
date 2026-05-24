636eab330de22bf09baef0e399589547fc0da1ba
Ahmed-Magdy-Elzayady <ahmed.m.elzayady@gmail.com>
2026-05-25 00:15:46 +0300

    Update Remote Validation

---
+++ Day04Lab/ViewModels/CategoryVM.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Day04Lab.ViewModels
+{
+    public class CategoryVM
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30,ErrorMessage ="Lenth Of Product name can not be more Than 30 characters")]
+        [MinLength(5,ErrorMessage = "Lenth Of Product name can not be less Than 5 characters")]
+        [Remote("IsNameAvailable", "Category",AdditionalFields ="Id")] 
+        public string Name { get; set; }
+
+    }
+}
